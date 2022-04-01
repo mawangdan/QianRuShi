423aa9376ddec653a398d559137f288a2ea3d7ef
mawangdan <mawangdan@foxmail.com>
2022-04-01 15:41:45 +0800

    commit

--- src/test.py
+++ src/test.py
@@ -12,16 +12,13 @@ def mqtt_connected(mqttClient, userdata, flags,rc):
         print("MQTT connect error:", rc)
 
 
-# 收到订阅主题数据回调函数
 def on_message(client, userdata, msg):
     print("主题:",msg.topic," 消息:")
     print(str(msg.payload.decode('utf-8')))
 
-# 订阅主题成功回调函数
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscrib topic success,qos = %d" % granted_qos)
 
-# 失去连接回调函数
 def on_disconnect(client, userdata, rc):
     if rc != 0:
         print("Unexpected disconnection %s" % rc)
